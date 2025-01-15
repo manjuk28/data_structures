8865559d7b1ef50e1723ce4398aef9bbb8e74e2b
Manju <kedareswari_gonuguntla@srmap.edu.in>
2025-01-15 16:32:23 +0530

    Update binary.c

--- lab_1/binary.c
+++ lab_1/binary.c
@@ -1,2 +1,39 @@
+#include <stdio.h>
 
-1st code
+void printBinary(int number) {
+    int binary[32]; 
+    int index = 0; 
+
+    // Handle special case for 0
+    if (number == 0) {
+        printf("0\n");
+        return;
+    }
+
+  
+    while (number > 0) {
+        binary[index++] = number % 2; 
+        number = number / 2;          
+    }
+
+ 
+    for (int i = index - 1; i >= 0; i--) {
+        printf("%d", binary[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int num;
+
+ 
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &num);
+
+    
+    printf("Binary representation of %d is: ", num);
+    printBinary(num);
+
+    return 0;
+}
+
